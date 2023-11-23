c183f4fa1c6b4fe6dcd9b3e237cc1fbda1f2c7dd
Gabriel Rizzi <75540693+Rizzi-dev@users.noreply.github.com>
2023-11-23 16:46:02 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -66,90 +66,73 @@ def sign_message(message, private_key):
 
 
 
-if __name__ == "__main__":
-    daemon = Pyro5.api.Daemon()
-    uri = daemon.register(Estoque())
 
-    keys = keysGenerator()
-    private_key = keys[0]
-    public_key = keys[1]
-    pk = keys[2]
+daemon = Pyro5.api.Daemon()
+uri = daemon.register(Estoque())
+keys = keysGenerator()
+private_key = keys[0]
+public_key = keys[1]
+pk = keys[2]
+servidor_nomes = Pyro5.api.locate_ns()
+server_uri = servidor_nomes.lookup("estoque")
+server = Pyro5.api.Proxy(server_uri) 
 
-    servidor_nomes = Pyro5.api.locate_ns()
-    server_uri = servidor_nomes.lookup("estoque")
-    server = Pyro5.api.Proxy(server_uri) 
-
- 
-    print("----- SEJA BEM VINDO AO SISTEMA DE ESTOQUE! -----")
-    print("-----           LOGIN DE USUÁRIO:           -----")
-    nome = input("NOME: ")
-    response = server.register_user(nome, public_key, uri)
-    print(response)
-
-    threading.Thread(target=daemon.requestLoop).start()
-
-    message = nome
-    signature = sign_message(message, pk)
-
-    #Menu de opções
-    while(True):
-        questions = [
-                inquirer.List('action', message="MENU", 
-                            choices=['Entrada de produtos', 'Saida de produtos', 'Relatorio'],)
-
-        ]
-        answer = inquirer.prompt(questions)
-        print(answer)
-        if(answer['action'] == 'Entrada de produtos'):
-                
-                codstr = input("Codigo do produto: ")
-                codigo = int(codstr)
-                name = input("Produto que deseja adicionar: ")
-                description = input("Descrição: ")
-                quantidadestr = input("Quantidade: ")
-                quantity = int(quantidadestr)
-                price = input("Qual o preço deste produto? : ")
-                server.record_entry(nome, codigo, name, description, quantity, price, 10, signature)
-                print("PRODUTO REGISTRADO COM SUCESSO")
-
-
-
-
-        elif(answer['action'] == 'Saida de produtos'):
-                print('Saida de produtos')
-                codsaidastr = input("Informe o codigo do produto: ")
-                codsaida = int(codsaidastr)
-                qntsaidastr = input("Informe a quantidade que deseja retirar: ")
-                qntsaida = int(qntsaidastr)
-                server.record_exit(codsaida, nome, qntsaida, signature)
-                print("SAÍDA EFETUADA COM SUCESSO PARA O PRODUTO:   " +name)
-
-
-
-        elif(answer['action'] == 'Relatorio'):
-                print('Relatorio')
-                questions2 = [
-                inquirer.List('action2', message="Qual acao deseja tomar?", 
-                            choices=['Produtos em estoque', 'Fluxo de movimentação', 'Lista de produtos sem saída'])
-
-                ]
-                answer = inquirer.prompt(questions2)
-                if(answer['action2']== 'Produtos em estoque'):
-                    print('Produtos em estoque')
-                    produtosEmEstoque= server.generate_stock_report('Produtos em estoque')
-                    for product in produtosEmEstoque:
-                        print(f"Produto {product['name']} - ({product['code']}) {product['quantity']} unidades em estoque")
-
-                elif(answer['action2']== 'Fluxo de movimentação'):
-                        print('Fluxo de movimentação')
-                        fluxoMov= server.generate_stock_report('Fluxo de movimentação')
-                        print("Movimentos:")
-                        for product in fluxoMov:
-                            for movement in product['movements']:
-                                print(f"Produto {product['name']} ({product['code']})  - Tipo: {movement['type']}, Quantidade: {movement['quantity']}, Hora: {movement['time']}")
-
-                elif(answer['action2']== 'Lista de produtos sem saída'):
-                        print('Lista de produtos sem saída')
-                        prodSemSaida = server.generate_stock_report('Lista de produtos sem saída')
-                        for product in prodSemSaida :
-                            print(f"Produto {product['name']} ({product['code']}) não teve movimentos de saída até 2 minutos atrás.")
+print("----- SEJA BEM VINDO AO SISTEMA DE ESTOQUE! -----")
+print("-----           LOGIN DE USUÁRIO:           -----")
+nome = input("NOME: ")
+response = server.register_user(nome, public_key, uri)
+print(response)
+threading.Thread(target=daemon.requestLoop).start()
+message = nome
+signature = sign_message(message, pk)
+#Menu de opções
+while(True):
+    questions = [
+            inquirer.List('action', message="MENU", 
+                        choices=['Entrada de produtos', 'Saida de produtos', 'Relatorio'],)
+    ]
+    answer = inquirer.prompt(questions)
+    print(answer)
+    if(answer['action'] == 'Entrada de produtos'):
+            
+            codstr = input("Codigo do produto: ")
+            codigo = int(codstr)
+            name = input("Produto que deseja adicionar: ")
+            description = input("Descrição: ")
+            quantidadestr = input("Quantidade: ")
+            quantity = int(quantidadestr)
+            price = input("Qual o preço deste produto? : ")
+            server.record_entry(nome, codigo, name, description, quantity, price, 10, signature)
+            print("PRODUTO REGISTRADO COM SUCESSO")
+    elif(answer['action'] == 'Saida de produtos'):
+            print('Saida de produtos')
+            codsaidastr = input("Informe o codigo do produto: ")
+            codsaida = int(codsaidastr)
+            qntsaidastr = input("Informe a quantidade que deseja retirar: ")
+            qntsaida = int(qntsaidastr)
+            server.record_exit(codsaida, nome, qntsaida, signature)
+            print("SAÍDA EFETUADA COM SUCESSO PARA O PRODUTO:   " +name)
+    elif(answer['action'] == 'Relatorio'):
+            print('Relatorio')
+            questions2 = [
+            inquirer.List('action2', message="Escolha um relatório para gerar: ", 
+                        choices=['Produtos em estoque', 'Fluxo de movimentação', 'Lista de produtos sem saída'])
+            ]
+            answer = inquirer.prompt(questions2)
+            if(answer['action2']== 'Produtos em estoque'):
+                print('Produtos em estoque')
+                produtosEmEstoque= server.generate_stock_report('Produtos em estoque')
+                for product in produtosEmEstoque:
+                    print(f"Produto {product['name']} - ({product['code']}) {product['quantity']} unidades em estoque")
+            elif(answer['action2']== 'Fluxo de movimentação'):
+                    print('Fluxo de movimentação')
+                    fluxoMov= server.generate_stock_report('Fluxo de movimentação')
+                    print("Movimentos:")
+                    for product in fluxoMov:
+                        for movement in product['movements']:
+                            print(f"Produto {product['name']} ({product['code']})  - Tipo: {movement['type']}, Quantidade: {movement['quantity']}, Hora: {movement['time']}")
+            elif(answer['action2']== 'Lista de produtos sem saída'):
+                    print('Lista de produtos sem saída')
+                    prodSemSaida = server.generate_stock_report('Lista de produtos sem saída')
+                    for product in prodSemSaida :
+                        print(f"Produto {product['name']} ({product['code']}) não teve movimentos de saída até 2 minutos atrás.")
